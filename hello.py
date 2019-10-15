c0f906eb2e8f61bcaa4d59e6d52e67a4aaad2482
ezorigo <ezorigo@users.noreply.github.com>
2019-10-15 12:14:10 -0400

    replicated lecture code

---
+++ hello.py
@@ -0,0 +1,17 @@
+# import flask package. flask makes app objects.
+from flask import Flask, render_template
+
+# create Flask web server, makes the application
+app = Flask(__name__)
+
+# routes determine location
+@app.route("/")
+
+# define simple function
+def home():
+    return render_template('home.html')
+
+@app.route("/about")
+
+def preds():
+    return render_template('about.html')
