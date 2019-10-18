4800e61cb7686a439a14c28430f4e32a5636e715
ezorigo <ezorigo@users.noreply.github.com>
2019-10-18 10:13:10 -0400

    minor changes

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -12,7 +12,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
-    #app.config['ENV'] = config('ENV') #should change this later to production
+    app.config['ENV'] = config('ENV') #should change this later to production
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
     DB.init_app(app)
@@ -24,7 +24,7 @@ def create_app():
 
     @app.route('/user', methods=['POST'])
     @app.route('/user/<name>', methods=['GET'])
-    def user(name=None,message=''):
+    def user(name=None, message=''):
         name = name or request.values['user_name']
         # import pdb; pdb.set_trace()
         try:
